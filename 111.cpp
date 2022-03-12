3a57a1056a2411940248ef6af96b84ccb1086bad
xingyu <capriccio_xy@sina.com>
2022-03-12 17:07:14 +0800

    x86 clocked

--- 111.cpp
+++ 111.cpp
@@ -5,28 +5,48 @@ const int N=10;
 int* res;
 int** mat;
 int* arr;
+long long head, tail, freq;  //timers
 
 void commonFunc()
 {
-    for(int j=0;j<N;j++)
+
+    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+    QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    int cir=1000;
+    for(int k=0;k<cir;k++)
     {
-        res[j]=0;
-        for(int i=0;i<N;i++)
-            res[j]+=mat[i][j]*arr[i];
+        for(int j=0;j<N;j++)
+        {
+            res[j]=0;
+            for(int i=0;i<N;i++)
+                res[j]+=mat[i][j]*arr[i];
+        }
     }
+    QueryPerformanceCounter((LARGE_INTEGER*)&tail );
+    cout<<( tail-head) * 1000.0 / freq /cir <<"ms"<<endl;
+
 }
 
 void cacheOptiFunc()
 {
-    for(int i=0;i<N;i++) res[i]=0;
-    for(int j=0;j<N;j++)
-        for(int i=0;i<N;i++)
-            res[i]+=mat[j][i]*arr[j];
+    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+    QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    int cir=1000;
+    for(int k=0;k<cir;k++)
+    {
+        for(int i=0;i<N;i++) res[i]=0;
+        for(int j=0;j<N;j++)
+            for(int i=0;i<N;i++)
+                res[i]+=mat[j][i]*arr[j];
+    }
+    QueryPerformanceCounter((LARGE_INTEGER*)&tail );
+    cout<<( tail-head) * 1000.0 / freq /cir <<"ms"<<endl;
 }
 
 
 int main()
 {
+
     mat=new int* [N];
     for(int i=0;i<N;i++) mat[i]=new int[N];
     for(int i=0;i<N;i++)
@@ -35,12 +55,14 @@ int main()
     arr=new int[N];
     res=new int[N];
     for(int i=0;i<N;i++) arr[i]=1;
-    commonFunc();
+
     cout<<"平凡算法："<<endl;
+    commonFunc();
     for(int i=0;i<N;i++) cout<<res[i]<<" ";
     cout<<endl;
-    cacheOptiFunc();
+
     cout<<"优化算法："<<endl;
+    cacheOptiFunc();
     for(int i=0;i<N;i++) cout<<res[i]<<" ";
 
 }
