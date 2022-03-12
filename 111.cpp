9f9881d4488417164bcd49ba04c310ebe1f735d5
xingyu <capriccio_xy@sina.com>
2022-03-12 16:35:33 +0800

    N=10,unclocked

--- 111.cpp
+++ 111.cpp
@@ -1,19 +1,46 @@
 #include<iostream>
+#include<windows.h>
 using namespace std;
-const int R=10;  //行数
-const int C=10;  //列数
+const int N=10;
+int* res;
+int** mat;
+int* arr;
+
 void commonFunc()
 {
+    for(int j=0;j<N;j++)
+    {
+        res[j]=0;
+        for(int i=0;i<N;i++)
+            res[j]+=mat[i][j]*arr[i];
+    }
+}
 
+void cacheOptiFunc()
+{
+    for(int i=0;i<N;i++) res[i]=0;
+    for(int j=0;j<N;j++)
+        for(int i=0;i<N;i++)
+            res[i]+=mat[j][i]*arr[j];
 }
-}
+
+
 int main()
 {
-    int* mat=new int[R];
-    for(int i=0;i<R;i++) mat[i]=new int[C];
-    for(int i=0;i<R;i++)
-        for(int j=0;j<C;j++)
-            mat[i][j]=1;    // 为方便验证正确性，将每一个元素设为1
-    int arr=new int[R];
-    for(int i=0;i<R;i++) arr[i]=1;
+    mat=new int* [N];
+    for(int i=0;i<N;i++) mat[i]=new int[N];
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            mat[i][j]=i+j;    // 为方便验证正确性，将每一个元素设为i+j
+    arr=new int[N];
+    res=new int[N];
+    for(int i=0;i<N;i++) arr[i]=1;
+    commonFunc();
+    cout<<"平凡算法："<<endl;
+    for(int i=0;i<N;i++) cout<<res[i]<<" ";
+    cout<<endl;
+    cacheOptiFunc();
+    cout<<"优化算法："<<endl;
+    for(int i=0;i<N;i++) cout<<res[i]<<" ";
+
 }
