fce91f9d3e5e6da53e1707de1183844613b63084
xingyu <capriccio_xy@sina.com>
2022-03-13 11:08:48 +0800

    linux, clocking bug fixed

--- linux .cpp
+++ linux .cpp
@@ -2,13 +2,14 @@
 #include<sys/time.h>
 #include<string>
 using namespace std;
-const int N=200;
+const int N=500;
 int res[N];
 int mat[N][N];
 int arr[N];
 
 void commonFunc()
 {
+    unsigned long diff;
     struct timeval tv_begin,tv_end;
     gettimeofday(&tv_begin,NULL);
     int cir=1000;
@@ -22,27 +23,29 @@ void commonFunc()
         }
     }
     gettimeofday(&tv_end,NULL);
-    cout<<(tv_begin.tv_usec - tv_end.tv_usec)<<endl;
-    cout<<-((tv_begin.tv_usec - tv_end.tv_usec)/1000.0)/cir<<"ms"<<endl;
+    diff = 1000000 * (tv_end.tv_sec-tv_begin.tv_sec)+ tv_end.tv_usec-tv_begin.tv_usec;
+    cout<<(diff/1000.0)/cir<<"ms"<<endl;
 
 }
 
 void cacheOptiFunc()
 {
+    unsigned long diff;
     struct timeval tv_begin,tv_end;
     gettimeofday(&tv_begin,NULL);
     int cir=1000;
     for(int k=0;k<cir;k++)
     {
-        //for(int i=0;i<N;i++) res[i]=0;
-        memset(res,0,sizeof(int)*N);
+        for(int i=0;i<N;i++) res[i]=0;
+        //memset(res,0,sizeof(int)*N);
         for(int j=0;j<N;j++)
             for(int i=0;i<N;i++)
                 res[i]+=mat[j][i]*arr[j];
     }
     gettimeofday(&tv_end,NULL);
-    cout<<(tv_begin.tv_usec - tv_end.tv_usec)<<endl;
-    cout<<-((tv_begin.tv_usec - tv_end.tv_usec)/1000.0)/cir<<"ms"<<endl;
+    diff = 1000000 * (tv_end.tv_sec-tv_begin.tv_sec)+ tv_end.tv_usec-tv_begin.tv_usec;
+    cout<<(diff/1000.0)/cir<<"ms"<<endl;
+
 }
 
 
