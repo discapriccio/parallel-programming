b9f77166d3ea75a08c79509530dddd4c95fde73b
xingyu <capriccio_xy@sina.com>
2022-05-01 19:19:54 +0800

    initially done

---
+++ ASG/ASG_x86/cmake-build-debug/CMakeFiles/ASG.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/xingyu/CLionProjects/ASG/source/Net.cpp" "C:/Users/xingyu/CLionProjects/ASG/cmake-build-debug/CMakeFiles/ASG.dir/source/Net.cpp.obj"
+  "C:/Users/xingyu/CLionProjects/ASG/source/Parser.cpp" "C:/Users/xingyu/CLionProjects/ASG/cmake-build-debug/CMakeFiles/ASG.dir/source/Parser.cpp.obj"
+  "C:/Users/xingyu/CLionProjects/ASG/source/Placement.cpp" "C:/Users/xingyu/CLionProjects/ASG/cmake-build-debug/CMakeFiles/ASG.dir/source/Placement.cpp.obj"
+  "C:/Users/xingyu/CLionProjects/ASG/source/main.cpp" "C:/Users/xingyu/CLionProjects/ASG/cmake-build-debug/CMakeFiles/ASG.dir/source/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
